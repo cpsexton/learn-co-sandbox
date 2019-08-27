9f40a94179adbae10c3ea1b9ebd5790bd9c61240
Christopher Sexton <cpsexton@gmail.com>
2019-08-27 20:05:13 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,27 @@
+=begin
+def word_count(sentence)
+ sentence.split().length
+end
+
+puts word_count("Hi, isn't this a great and interesting sentence??")
+
+
+
+def rude_greeting(name=nil)
+ name ||= "you jerk"
+ puts "Hey there, #{name}"
+end
+print rude_greeting
+
+best_animal = "cat"
+favorite_animal = best_animal
+puts favorite_animal
+=end
+
+foods = {"pie" => "delicious", "broccoli" => "not delicious",
+"carrots" => "not delicious", "apples" => "delicious",
+"peanut butter" => "delicious"}
+
+def get_delicious(foods)
+  
+end
